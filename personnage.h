ae4a94d7c5fb77dea503cbc29ebc5b0c77e2348f
fatucode <wannjalloh@gmail.com>
2025-05-06 12:46:43 +0200

    Update personnage.h

--- personnage.h
+++ personnage.h
@@ -1,25 +1,29 @@
+
 #ifndef PERSONNAGE_H
 #define PERSONNAGE_H
 
-#define MAX_NOM 50
-#define MAX_TYPE 20
-
+// Définition de la structure Ninja
 typedef struct {
-    char nom[MAX_NOM];
-    char type[MAX_TYPE];
-    int PV;
+    char nom[50];
+    char type[20];
     int PV_max;
+    int PV;
     int attaque;
     int defense;
     int vitesse;
     int agilite;
     int nb_techniques;
     int nb_effets;
-    // Tu peux ajouter des tableaux de techniques/effets plus tard
+    // Effet temporaire (ex. : bouclier ou soin actif pour ce tour)
+    int bouclier_actif;   // 1 si actif, 0 sinon
+    int soin_ameliore;    // 1 si effet boisson magique actif
+} Ninja;
 } Ninja;
 
+// Déclarations des fonctions
 Ninja creerPersonnage(const char *nom, const char *type);
 void afficherPersonnage(Ninja p);
 int est_vivant(Ninja p);
+Ninja saisirPersonnage();  // Fonction pour saisir un personnage
 
 #endif
