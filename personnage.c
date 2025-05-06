bc93bf39371be831c3e75f07c76a6c66b153519f
fatucode <wannjalloh@gmail.com>
2025-05-06 12:47:17 +0200

    Update personnage.c

--- personnage.c
+++ personnage.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "personnage.h"
- 
+
 // Crée un personnage selon son type : attaquant, défenseur ou guérisseur
 Ninja creerPersonnage(const char *nom, const char *type) {
     Ninja p;
     strcpy(p.nom, nom);
     strcpy(p.type, type);
     p.PV_max = 100;
- 
+
     if (strcmp(type, "attaquant") == 0) {
         p.attaque = 30;
         p.defense = 10;
@@ -32,16 +32,30 @@ Ninja creerPersonnage(const char *nom, const char *type) {
         p.vitesse = 10;
         p.agilite = 10;
     }
- 
+
     p.PV = p.PV_max;
- 
+
     // Initialisation vide des techniques et effets spéciaux
     p.nb_techniques = 0;
     p.nb_effets = 0;
- 
+
     return p;
 }
- 
+// Permet à l’utilisateur de saisir un personnage
+Ninja saisirPersonnage() {
+    char nom[50];
+    char type[20];
+
+    printf("Entrez le nom du personnage : ");
+    scanf("%49s", nom);
+
+    printf("Entrez le type de personnage (attaquant / défenseur / guérisseur) : ");
+    scanf("%19s", type);
+
+    return creerPersonnage(nom, type);
+}
+
+
 // Affiche les informations d’un personnage
 void afficherPersonnage(Ninja p) {
     printf("Nom : %s\n", p.nom);
@@ -52,8 +66,10 @@ void afficherPersonnage(Ninja p) {
     printf("Vitesse : %d\n", p.vitesse);
     printf("Agilité : %d\n", p.agilite);
 }
- 
+
 // Vérifie si le personnage est encore en vie
 int est_vivant(Ninja p) {
     return p.PV > 0;
 }
+
+
