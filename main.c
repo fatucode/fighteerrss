c28364384398cbe091d23184ab3c86272313c4d1
fatucode <wannjalloh@gmail.com>
2025-05-06 12:43:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include "personnage.h"
+#include "jeu.h"
+#include "combat.h"
 
 int main() {
-    printf("Hello fighters!\n");
+    // Initialisation des équipes
+    Ninja equipe1[3] = { ... };  // Remplir avec les données des ninjas
+    Ninja equipe2[3] = { ... };  // Remplir avec les données des ninjas
+
+    // Démarrer la partie
+    jouer_partie(equipe1, equipe2);
+
     return 0;
 }
