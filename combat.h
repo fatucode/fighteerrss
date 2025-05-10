49c7c6ec11029522e92f57160de63192197b3652
fatucode <wannjalloh@gmail.com>
2025-05-10 10:13:01 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -1,12 +1,8 @@
 #ifndef COMBAT_H
 #define COMBAT_H
-// Attaquant : effectue deux attaques successives sur la même cible
 void attaquer(Personnage *attaquant, Personnage *cible);
-vois soigner(Personnage *guerisseur ,Personnage *cible)
+vois soigner(Personnage *guerisseur ,Personnage *cible);
 void double_attaque(Personnage *attaquant, Personnage *cible);
-
-// Défenseur : applique un effet de bouclier à tous les membres de l'équipe (réduction des dégâts pendant 1 tour)
-void bouclier_equipe(Personnage equipe[], int taille);
-
-// Guérisseur : soigne un allié ou lui-même avec un soin renforcé
 void boisson_magique(Personnage *soigneur, Personnage *cible);
+void bouclier_equipe(Personnage *defenseur, Personnage equipe[], int taille) ;
+void utiliser_competence(Personnage *perso, Personnage equipe[], int taille, Personnage *cible) ;
