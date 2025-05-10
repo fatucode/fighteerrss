157735c0e43b82cc33639c06651299745035a6a2
fatucode <wannjalloh@gmail.com>
2025-05-10 10:15:00 +0200

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -4,190 +4,79 @@
 #include "personnage.h"
 
 
-void initialiser_jeu() {
-    // Créer équipe 1
-    Ninja equipe1[3];
-    printf("Création de l'équipe 1 :\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Création du personnage %d :\n", i + 1);
-        equipe1[i] = saisir_personnage();  // Appel à la fonction de saisie
-    }
-
-    // Créer équipe 2
-    Ninja equipe2[3];
-    printf("Création de l'équipe 2 :\n");
-    for (int i = 0; i < 3; i++) {
-        printf("Création du personnage %d :\n", i + 1);
-        equipe2[i] = saisir_personnage();  // Appel à la fonction de saisie
-    }
-
-    // Affichage des équipes créées
-    printf("Équipe 1 :\n");
-    for (int i = 0; i < 3; i++) {
-        afficherPersonnage(equipe1[i]);
-    }
-
-    printf("Équipe 2 :\n");
-    for (int i = 0; i < 3; i++) {
-        afficherPersonnage(equipe2[i]);
-    }
-}
-
-
-int equipe_a_guerisseur(Ninja equipe[], int taille_equipe) {
-    for (int i = 0; i < taille_equipe; i++) {
-        if (strcmp(equipe[i].type, "guérisseur") == 0) {
-            return 1; // Il y a un guérisseur
+void afficher_equipes(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2) {
+    printf("\n=== EQUIPE 1 ===\n");
+    for (int i = 0; i < taille1; i++) {
+        if (equipe1[i].PV > 0) {
+            afficherPersonnage(equipe1[i]);
         }
     }
-    return 0; // Pas de guérisseur
+
+    printf("\n=== EQUIPE 2 ===\n");
+    for (int i = 0; i < taille2; i++) {
+        if (equipe2[i].PV > 0) {
+            afficherPersonnage(equipe2[i]);
+        }
+    }
 }
-void jouer_partie() {
+
+void jouer_tour(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2) {
+    // Tour equipe 1
+    for (int i = 0; i < taille1; i++) {
+        if (equipe1[i].PV <= 0) continue;
+
+        printf("\n--- Tour de %s ---\n", equipe1[i].nom);
+        afficher_equipes(equipe1, taille1, equipe2, taille2);
+
+        int choix;
+        printf("\n1. Attaquer\n2. Competence\n3. Soigner\nChoix : ");
+        scanf("%d", &choix);
+
+        if (choix == 1) {
+            printf("Choisissez une cible : ");
+            int cible;
+            scanf("%d", &cible);
+            attaquer(&equipe1[i], &equipe2[cible-1]);
+        } 
+        else if (choix == 2) {
+            printf("Choisissez une cible : ");
+            int cible;
+            scanf("%d", &cible);
+            utiliser_competence(&equipe1[i], equipe1, taille1, &equipe2[cible-1]);
+        } 
+        else if (choix == 3 && strcmp(equipe1[i].type, "guerisseur") == 0) {
+            printf("Choisissez un allie : ");
+            int allie;
+            scanf("%d", &allie);
+            soigner(&equipe1[i], &equipe1[allie-1]);
+        }
+    }
+}
+
+Personnage* jouer_partie(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2) {
+    int tour = 1;
+    
     while (1) {
-        printf("\n========== Nouveau Tour ==========\n");
-
-        int choix, index_attaquant, perso_a_attaquer, perso_a_soigner;
-
-        // ==== TOUR DE L'ÉQUIPE 1 ====
-        printf("\n--- Tour de l'Équipe 1 ---\n");
-
-        if (equipe_a_guerisseur(equipe1, 3)) {
-            printf("Choisissez une action :\n");
-            printf("1. Attaquer\n");
-            printf("2. Soigner\n");
-            printf("3. Utiliser une compétence spéciale\n");
-            printf("Votre choix : ");
-            while (scanf("%d", &choix) != 1 || (choix < 1 || choix > 3)) {
-                while (getchar() != '\n');
-                printf("Choix invalide. Veuillez choisir 1, 2 ou 3 : ");
-            }
-        } else {
-            printf("Choisissez une action :\n");
-            printf("1. Attaquer\n");
-            printf("Votre choix : ");
-            while (scanf("%d", &choix) != 1 || choix != 1) {
-                while (getchar() != '\n');
-                printf("Choix invalide. Veuillez choisir 1 : ");
-            }
-            printf("Vous n'avez pas de guérisseur, option 'Soigner' désactivée.\n");
+        printf("\n=== TOUR %d ===\n", tour);
+        
+        jouer_tour(equipe1, taille1, equipe2, taille2);
+        
+        // Vérifier si equipe2 est morte
+        int equipe2_vivante = 0;
+        for (int i = 0; i < taille2; i++) {
+            if (equipe2[i].PV > 0) equipe2_vivante = 1;
         }
-
-        printf("Quel ninja de votre équipe voulez-vous utiliser ? (1-3) : ");
-        scanf("%d", &index_attaquant);
-        Ninja *joueur_attaquant = &equipe1[index_attaquant - 1];
-
-        // Traitement de l'action choisie
-        if (choix == 1) {
-            // Attaque
-            printf("Qui voulez-vous attaquer ? (1-3) :\n");
-            for (int i = 0; i < 3; i++) {
-                printf("%d. ", i + 1);
-                afficherPersonnage(equipe2[i]);
-            }
-            scanf("%d", &perso_a_attaquer);
-            Ninja *cible = &equipe2[perso_a_attaquer - 1];
-            attaquer(joueur_attaquant, cible);
-        } else if (choix == 2) {
-            // Soigner
-            printf("Qui voulez-vous soigner ? (1-3) :\n");
-            for (int i = 0; i < 3; i++) {
-                printf("%d. ", i + 1);
-                afficherPersonnage(equipe1[i]);
-            }
-            scanf("%d", &perso_a_soigner);
-            Ninja *cible_soin = &equipe1[perso_a_soigner - 1];
-            soigner(joueur_attaquant, cible_soin);
-        } else if (choix == 3) {
-            // Utilisation de la technique spéciale
-            if (strcmp(joueur_attaquant->type, "attaquant") == 0) {
-                printf("%s utilise la technique spéciale 'Double Attaque'!\n", joueur_attaquant->nom);
-                double_attaque(joueur_attaquant);
-            } else if (strcmp(joueur_attaquant->type, "defenseur") == 0) {
-                printf("%s utilise la technique spéciale 'Bouclier'!\n", joueur_attaquant->nom);
-                bouclier(joueur_attaquant);
-            } else if (strcmp(joueur_attaquant->type, "guerisseur") == 0) {
-                printf("%s utilise la technique spéciale 'Boisson Magique'!\n", joueur_attaquant->nom);
-                boisson_magique(joueur_attaquant, joueur_attaquant);  // Guérisseur peut se soigner lui-même ou un autre
-            } else {
-                printf("Aucune compétence spéciale disponible pour ce personnage.\n");
-            }
+        if (!equipe2_vivante) return equipe1;
+        
+        jouer_tour(equipe2, taille2, equipe1, taille1);
+        
+        // Vérifier si equipe1 est morte
+        int equipe1_vivante = 0;
+        for (int i = 0; i < taille1; i++) {
+            if (equipe1[i].PV > 0) equipe1_vivante = 1;
         }
-
-        // ==== TOUR DE L'ÉQUIPE 2 ====
-        printf("\n--- Tour de l'Équipe 2 ---\n");
-
-        if (equipe_a_guerisseur(equipe2, 3)) {
-            printf("Choisissez une action :\n");
-            printf("1. Attaquer\n");
-            printf("2. Soigner\n");
-            printf("3. Utiliser une compétence spéciale\n");
-            printf("Votre choix : ");
-            while (scanf("%d", &choix) != 1 || (choix < 1 || choix > 3)) {
-                while (getchar() != '\n');
-                printf("Choix invalide. Veuillez choisir 1, 2 ou 3 : ");
-            }
-        } else {
-            printf("Choisissez une action :\n");
-            printf("1. Attaquer\n");
-            printf("Votre choix : ");
-            while (scanf("%d", &choix) != 1 || choix != 1) {
-                while (getchar() != '\n');
-                printf("Choix invalide. Veuillez choisir 1 : ");
-            }
-            printf("Vous n'avez pas de guérisseur, option 'Soigner' désactivée.\n");
-        }
-
-        printf("Quel ninja de votre équipe voulez-vous utiliser ? (1-3) : ");
-        scanf("%d", &index_attaquant);
-        joueur_attaquant = &equipe2[index_attaquant - 1];
-
-        // Traitement de l'action choisie pour l'équipe 2
-        if (choix == 1) {
-            // Attaque
-            printf("Qui voulez-vous attaquer ? (1-3) :\n");
-            for (int i = 0; i < 3; i++) {
-                printf("%d. ", i + 1);
-                afficherPersonnage(equipe1[i]);
-            }
-            scanf("%d", &perso_a_attaquer);
-            Ninja *cible = &equipe1[perso_a_attaquer - 1];
-            attaquer(joueur_attaquant, cible);
-        } else if (choix == 2) {
-            // Soigner
-            printf("Qui voulez-vous soigner ? (1-3) :\n");
-            for (int i = 0; i < 3; i++) {
-                printf("%d. ", i + 1);
-                afficherPersonnage(equipe2[i]);
-            }
-            scanf("%d", &perso_a_soigner);
-            Ninja *cible_soin = &equipe2[perso_a_soigner - 1];
-            soigner(joueur_attaquant, cible_soin);
-        } else if (choix == 3) {
-            // Utilisation de la technique spéciale
-            if (strcmp(joueur_attaquant->type, "attaquant") == 0) {
-                printf("%s utilise la technique spéciale 'Double Attaque'!\n", joueur_attaquant->nom);
-                double_attaque(joueur_attaquant);
-            } else if (strcmp(joueur_attaquant->type, "defenseur") == 0) {
-                printf("%s utilise la technique spéciale 'Bouclier'!\n", joueur_attaquant->nom);
-                bouclier(joueur_attaquant);
-            } else if (strcmp(joueur_attaquant->type, "guerisseur") == 0) {
-                printf("%s utilise la technique spéciale 'Boisson Magique'!\n", joueur_attaquant->nom);
-                boisson_magique(joueur_attaquant, joueur_attaquant);  // Guérisseur peut se soigner lui-même ou un autre
-            } else {
-                printf("Aucune compétence spéciale disponible pour ce personnage.\n");
-            }
-        }
-
-        // Vérification de la victoire après chaque tour
-        if (verifier_victoire() == 1) {
-            printf("L'équipe 1 a gagné!\n");
-            break;
-        }
-        if (verifier_victoire() == 2) {
-            printf("L'équipe 2 a gagné!\n");
-            break;
-        }
+        if (!equipe1_vivante) return equipe2;
+        
+        tour++;
     }
 }
-
-
