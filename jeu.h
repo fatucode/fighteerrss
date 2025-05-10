74f19fa622876b1c21d41df4422ef19dba66b7f9
fatucode <wannjalloh@gmail.com>
2025-05-10 10:15:49 +0200

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,5 +1,9 @@
 #ifndef JEU_H
 #define JEU_H
-void initialiser_jeu();
-int equipe_a_guerisseur(Ninja equipe[], int taille_equipe);
-void jouer_partie();
+void afficher_equipes(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
+
+void jouer_tour(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
+   
+
+Personnage* jouer_partie(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
+  
