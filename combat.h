5dac19bdbb375847c14ade2759750c55a4ca67a0
fatucode <wannjalloh@gmail.com>
2025-05-11 23:22:17 +0200

    Update combat.h

--- combat.h
+++ combat.h
@@ -2,12 +2,13 @@
 #define COMBAT_H
 #include "jeu.h"
 #include "personnage.h"
+#include <stdbool.h>
 
 void utiliser_competence(Personnage* utilisateur, Personnage equipe[], int taille, Personnage* cible);
 void attaquer(Personnage* attaquant, Personnage* cible);
 void soigner(Personnage* soigneur, Personnage* cible);
 void double_attaque(Personnage *attaquant, Personnage *cible);
 void bouclier_equipe(Personnage *defenseur, Personnage equipe[], int taille);
-void boisson_magique(Personnage* soigneur, Personnage* cible);
+bool ressusciter(Personnage *guerisseur, Personnage equipe[], int taille);
 
 #endif
