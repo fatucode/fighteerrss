905a7d85dd474726275541a76558e81b6fe2b9db
fatucode <wannjalloh@gmail.com>
2025-05-11 03:32:20 +0200

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -1,10 +1,8 @@
 #ifndef JEU_H
 #define JEU_H
+#include "personnage.h"
 void afficher_equipes(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
-
 void jouer_tour(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
-   
-
 Personnage* jouer_partie(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
   
 #endif
