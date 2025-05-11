3ba4f0ee7578e6fed02a6c0cb35fe087b18eb691
fatucode <wannjalloh@gmail.com>
2025-05-11 11:35:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,59 +6,7 @@
 #include "combat.h"
 #include "personnage.h"
 
-void afficherMenuCombattants(Personnage persos[], int count, const char* type) {
-    printf("\n=== Choix %s ===\n", type);
-    for (int i = 0; i < count; i++) {
-        printf("%d. %s (Att:%d Def:%d Vit:%d Agi:%d)\n", 
-              i+1, persos[i].nom, persos[i].attaque, persos[i].defense,
-              persos[i].vitesse, persos[i].agilite);
-    }
-}
 
-Personnage choisirPersoParType(const char* filename, const char* type) {
-    FILE* file = fopen(filename, "r");
-    Personnage persos[3]; // Max 3 persos par type
-    int count = 0;
-    char line[256];
-
-    while (fgets(line, sizeof(line), file) && count < 3) {
-        Personnage p;
-        sscanf(line, "%[^;];%[^;];%d;%d;%d;%d;%[^;];%d;%d",
-               p.nom, p.type, &p.attaque, &p.defense,
-               &p.vitesse, &p.agilite, p.competence.nom,
-               &p.competence.type, &p.competence.duree);
-
-        if (strcmp(p.type, type) == 0) {
-            persos[count++] = p;
-        }
-    }
-    fclose(file);
-
-    afficherMenuCombattants(persos, count, type);
-
-    int choix;
-    do {
-        printf("Choisissez votre %s (1-%d): ", type, count);
-        scanf("%d", &choix);
-    } while (choix < 1 || choix > count);
-
-    return persos[choix-1];
-}
-
-void genererEquipeEnnemie(Personnage equipe[], const char* filename) {
-    // Compter le nombre total de persos
-    FILE* file = fopen(filename, "r");
-    int totalPersos = 0;
-    while (fgets(line, sizeof(line), file) totalPersos++;
-    fclose(file);
-
-    // Choisir 3 persos aléatoires
-    srand(time(NULL));
-    for (int i = 0; i < 3; i++) {
-        int id = rand() % totalPersos;
-        equipe[i] = chargerCombattant(filename, id);
-    }
-}
 
 int main() {
     printf("=== NINJA COMBAT ===\n\n");
