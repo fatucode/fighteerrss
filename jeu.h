ce3be9b1c132cb2a2892f9e41d6a9c8c441f9c58
fatucode <wannjalloh@gmail.com>
2025-05-11 12:33:31 +0200

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -4,6 +4,6 @@
 void afficher_equipes(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
 void jouer_tour(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
 Personnage* jouer_partie(Personnage equipe1[], int taille1, Personnage equipe2[], int taille2);
-fin_tour(Personnage equipe[], int taille)
+fin_tour(Personnage equipe[], int taille);s
   
 #endif
